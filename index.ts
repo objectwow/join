88edfeeb3b0f0df9dc152e27000cae7f360a221b
vtuanjs <vantuan130393@gmail.com>
2024-10-09 22:56:26 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { joinData } from "./core";
---
+++ util.ts
@@ -0,0 +1,19 @@
+export enum Types {
+	Object = 'Object',
+	Array = 'Array',
+	Number = 'Number',
+	String = 'String',
+	Undefined = 'Undefined',
+	Null = 'Null',
+	Date = 'Date',
+	Boolean = 'Boolean',
+	RegExp = 'RegExp',
+	Function = 'Function',
+  }
+  
+export const typeOf = (value: any): Types =>
+	Object.prototype.toString.call(value).slice(8, -1) as Types;
+
+export function isNullOrUndefined(value: any){
+	return value === null || value === undefined;
+}
